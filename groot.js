637b35ea867706df52ca7aff3cde928e5145d25b
Rhin0s <jannis.guhl@gmail.com>
2016-12-06 13:38:28 +0100

    Bot replies now

--- groot.js
+++ groot.js
@@ -2,13 +2,14 @@ var Discord = require("discord.js");
 var bot = new Discord.Client();
 
 bot.on("message", msg => {
-	if (msg.content.startsWith("?")) {
-		msg.channel.sendMessage("I am Groot!");
+	if(msg.author.bot) return; 
+	if (msg.content.includes("groot") || msg.content.includes("Groot")) {
+		msg.reply('ich bin Groot!');
 	}
 });
 
 bot.on('ready', () => {
-  console.log('I am Groot!');
+  console.log('Ich bin Groot!');
 });
 
 bot.login("MjU1NjUwMzY0MDY4NDYyNTk0.CyguSA.RoOd5dnlnp9w1uJ3IYF0gnI9h7A");
