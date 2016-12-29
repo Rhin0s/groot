5f3057bc7f8213a6e291d39b6920ed9a9976114e
Rhin0s <jannis.guhl@gmail.com>
2016-12-29 13:49:17 +0100

    Auth-Token ausgelagert

--- groot.js
+++ groot.js
@@ -1,9 +1,17 @@
 var Discord = require("discord.js");
 var bot = new Discord.Client();
 
+//Authentifierungsdaten aus Datei laden
+try {
+        var authFile = require("./auth.json");
+} catch (e){
+        console.log("Die auth.json-Datei fehlt!\n"+e.stack);
+        process.exit();
+}
+
 bot.on("message", msg => {
 	if(msg.author.bot) return; 
-	if (msg.content.includes("groot") || msg.content.includes("Groot")) {
+	if (msg.content.toLowerCase().match(/[g]+[r]+[o]{2,}[t]+/g)) {
 		msg.reply('ich bin Groot!');
 	}
 });
@@ -12,4 +20,4 @@ bot.on('ready', () => {
   console.log('Ich bin Groot!');
 });
 
-bot.login("MjU1NjUwMzY0MDY4NDYyNTk0.CyguSA.RoOd5dnlnp9w1uJ3IYF0gnI9h7A");
+bot.login(authFile.bot_token);
